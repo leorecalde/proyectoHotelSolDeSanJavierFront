8ae45f648748aacccc42ee72ddda380133755b85
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-10-19 13:43:32 -0300

    ✅ [ADD] estroctura basica de react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavbarComponent from "./components/common/Navbar/NavbarComponent";
+import Index from "./components/pages/Index";
 
 function App() {
-  
-
   return (
     <>
-      
+      <BrowserRouter>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
