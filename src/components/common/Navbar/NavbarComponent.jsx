a36473212f79d1f89b810e99ff1af1036f647910
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-10-19 14:07:51 -0300

    🎨 [STYLE] se incorporo varibles de estilos globales

--- src/components/common/Navbar/NavbarComponent.jsx
+++ src/components/common/Navbar/NavbarComponent.jsx
@@ -2,10 +2,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import style from "./NavbarComponent.module.css";
 
 const NavbarComponent = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={`bg-body-tertiary ${style.navbar}`}>
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
