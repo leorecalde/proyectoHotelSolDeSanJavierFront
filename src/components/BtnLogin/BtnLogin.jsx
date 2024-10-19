c3c5e255d67ccd96db4f655842ed56a23c0233de
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-10-19 20:28:50 -0300

    💫 [FEAT] navbar adaptado para versiones desktop

--- src/components/BtnLogin/BtnLogin.jsx
+++ src/components/BtnLogin/BtnLogin.jsx
@@ -2,7 +2,9 @@ import style from "./BtnLogin.module.css";
 const BtnLogin = () => {
   return (
     <>
-      <button className={`mt-2 ${style.btn_login} `}>Log in</button>
+      <button className={`mt-2 ms-lg-3 mt-lg-0 ${style.btn_login} `}>
+        Log in
+      </button>
     </>
   );
 };
--- src/components/common/Navbar/NavbarComponent.jsx
+++ src/components/common/Navbar/NavbarComponent.jsx
@@ -2,15 +2,37 @@ import Container from "react-bootstrap/Container";
 import style from "./NavbarComponent.module.css";
 import BtnReservation from "../../BtnReservation/BtnReservation";
 import logo from "../../../assets/logo.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BtnLogin from "../../BtnLogin/BtnLogin";
 
 const NavbarComponent = () => {
   const [menuIsActive, setMenuIsActive] = useState(false);
+  const [widthWindowState, setWidthWindowState] = useState(window.innerWidth);
+  const [currentLink, setCurrentLink] = useState(
+    sessionStorage.getItem("currentLink") || "home"
+  );
 
+  const handleResizeWindow = () => {
+    let newWidth = window.innerWidth;
+    setWidthWindowState(newWidth);
+  };
+  const handleLink = (currentLinkParam) => {
+    sessionStorage.setItem("currentLink", currentLinkParam);
+    setCurrentLink(sessionStorage.getItem("currentLink"));
+    setMenuIsActive(!menuIsActive);
+  };
   const toggleMenu = () => {
     setMenuIsActive(!menuIsActive);
   };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResizeWindow);
+
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, [widthWindowState]);
+
   return (
     <nav className={`navbar navbar-expand-lg ${style.navbar}`}>
       <Container>
@@ -32,39 +54,102 @@ const NavbarComponent = () => {
           </a>
         </div>
 
-        <BtnReservation />
+        {widthWindowState < 992 && <BtnReservation />}
 
         <div
           className={` ${style.collapse_navbar} ${
             menuIsActive ? style.navbar_collapse_active : ""
           } `}
         >
-          <ul className="navbar-nav m-auto text-center">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
+          <ul className="navbar-nav text-center">
+            <li
+              className="nav-item"
+              onClick={() => {
+                handleLink("experiencias");
+              }}
+            >
+              <a
+                className={`nav-link ${style.navbar_link} ${
+                  currentLink === "experiencias" && style.active
+                } `}
+              >
                 Experiencias
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
+            <li
+              className="nav-item"
+              onClick={() => {
+                handleLink("habitaciones");
+              }}
+            >
+              <a
+                className={`nav-link ${style.navbar_link} ${
+                  currentLink === "habitaciones" && style.active
+                } `}
+              >
                 Habitaciones
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link">Promo</a>
+            <li
+              className="nav-item"
+              onClick={() => {
+                handleLink("promos");
+              }}
+            >
+              <a
+                className={`nav-link ${style.navbar_link} ${
+                  currentLink === "promos" && style.active
+                } `}
+              >
+                Promos
+              </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link">Actividades</a>
+            <li
+              className="nav-item"
+              onClick={() => {
+                handleLink("actividades");
+              }}
+            >
+              <a
+                className={`nav-link ${style.navbar_link} ${
+                  currentLink === "actividades" && style.active
+                }`}
+              >
+                Actividades
+              </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link">Galería</a>
+            <li
+              className="nav-item"
+              onClick={() => {
+                handleLink("galeria");
+              }}
+            >
+              <a
+                className={`nav-link ${style.navbar_link} ${
+                  currentLink === "galeria" && style.active
+                }`}
+              >
+                Galería
+              </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link">Contacto</a>
+            <li
+              className="nav-item"
+              onClick={() => {
+                handleLink("contacto");
+              }}
+            >
+              <a
+                className={`nav-link ${style.navbar_link} ${
+                  currentLink === "contacto" && style.active
+                } `}
+              >
+                Contacto
+              </a>
             </li>
             <BtnLogin />
           </ul>
         </div>
+        {widthWindowState >= 992 && <BtnReservation />}
       </Container>
     </nav>
   );
