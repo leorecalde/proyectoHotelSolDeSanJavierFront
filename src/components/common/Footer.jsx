9590f37d5fe6bd006188ad54b5111732e752284d
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-10-20 18:31:31 -0300

    corregi un mal tipeo en el footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -11,33 +11,29 @@ const Footer = () => {
           <Col md={4}>
             <h5 className='fs-3'>Navegación</h5>
             <ul className="list-unstyled">
-              <li><a href="/" className="text-warning fs-5">Inicio</a></li>
-              <li><a href="/about" className="text-warning fs-5">Sobre Nosotros</a></li>
-              <li><a href="/services" className="text-warning fs-5">Servicios</a></li>
-              <li><a href="/contact" className="text-warning fs-5">Contacto</a></li>
+              <li><a href="/inicio" className="text-warning fs-5">Inicio</a></li>
+              <li><a href="/nosotros" className="text-warning fs-5">Sobre Nosotros</a></li>
+              <li><a href="/servicios" className="text-warning fs-5">Servicios</a></li>
+              <li><a href="/contacto" className="text-warning fs-5">Contacto</a></li>
             </ul>
           </Col>
-
           <Col md={4}>
       <h5 className='fs-3'>Síguenos</h5>
       <ul className="list-unstyled">
         <li>
           <a href="https://www.facebook.com" className="text-light fs-5">
-            {/* Ícono de Facebook */}
             <i className="bi bi-facebook icon"></i>
             Facebook
           </a>
         </li>
         <li>
           <a href="https://www.twitter.com" className="text-light fs-5">
-            {/* Ícono de Twitter */}
             <i className="bi bi-twitter icon"></i>
             Twitter
           </a>
         </li>
         <li>
           <a href="https://www.instagram.com" className="text-light fs-5">
-            {/* Ícono de Instagram */}
             <i className="bi bi-instagram icon"></i>
             Instagram
           </a>
