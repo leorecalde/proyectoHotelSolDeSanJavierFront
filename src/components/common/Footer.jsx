6f28a241068c7460bb1880fd939ef133cf528c27
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-10-22 18:09:49 -0300

    agregue una imagen del hotel  en el footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,51 +1,82 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import './Footer.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./Footer.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-light py-5">
+    <footer className="text-light py-5 footer-background">
       <Container>
         <Row>
           <Col md={4}>
-            <h5 className='fs-3'>Navegación</h5>
+            <h5 className="text-dark fs-3">Navegación</h5>
             <ul className="list-unstyled">
-              <li><a href="/inicio" className="text-warning fs-5">Inicio</a></li>
-              <li><a href="/nosotros" className="text-warning fs-5">Sobre Nosotros</a></li>
-              <li><a href="/servicios" className="text-warning fs-5">Servicios</a></li>
-              <li><a href="/contacto" className="text-warning fs-5">Contacto</a></li>
+              <li>
+                <a href="/inicio" className="text-success-emphasis fs-5">
+                  Inicio
+                </a>
+              </li>
+              <li>
+                <a href="/nosotros" className="text-success-emphasis fs-5">
+                  Sobre Nosotros
+                </a>
+              </li>
+              <li>
+                <a href="/servicios" className="text-success-emphasis fs-5">
+                  Servicios
+                </a>
+              </li>
+              <li>
+                <a href="/contacto" className="text-success-emphasis fs-5">
+                  Contacto
+                </a>
+              </li>
             </ul>
           </Col>
           <Col md={4}>
-      <h5 className='fs-3'>Síguenos</h5>
-      <ul className="list-unstyled">
-        <li>
-          <a href="https://www.facebook.com" className="text-light fs-5">
-            <i className="bi bi-facebook icon"></i>
-            Facebook
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" className="text-light fs-5">
-            <i className="bi bi-twitter icon"></i>
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com" className="text-light fs-5">
-            <i className="bi bi-instagram icon"></i>
-            Instagram
-          </a>
-        </li>
-      </ul>
-    </Col>
+            <h5 className="text-dark fs-3">Síguenos</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a
+                  href="https://www.facebook.com"
+                  className="text-success-emphasis fs-5"
+                >
+                  <i className="bi bi-facebook icon"></i>
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.twitter.com"
+                  className="text-success-emphasis fs-5"
+                >
+                  <i className="bi bi-twitter icon"></i>
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com"
+                  className="text-success-emphasis fs-5"
+                >
+                  <i className="bi bi-instagram icon"></i>
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </Col>
 
           <Col md={4}>
-            <h5 className='fs-3'>Contacto</h5>
-            <p className='fs-5'>Dirección: Ruta 340 - Km 23, San Javier.</p>
-            <p className='fs-5'>Email: reservas@hotelsolsanjavier.com.ar</p>
-            <p className='fs-5'>Teléfono: (+54 9) (0381) 155-279796.</p>
+            <h5 className="text-dark fs-3">Contacto</h5>
+            <p className="text-success-emphasis fs-5">
+              Dirección:Ruta 340 - Km 23, San Javier,Tucuman
+            </p>
+            <p className="text-success-emphasis fs-5">
+              Email:reservas@hotelsolsanjavier.com.ar
+            </p>
+            <p className="text-success-emphasis fs-5">
+              Teléfono:(+54 9) (0381) 155-279796.
+            </p>
           </Col>
         </Row>
         <Row className="text-center mt-3">
@@ -56,6 +87,6 @@ const Footer = () => {
       </Container>
     </footer>
   );
-}
+};
 
 export default Footer;
