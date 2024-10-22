79e60be1bedc70094477f8c5afd78faac1ee8033
Leonardo Recalde <leo03recalde@gmail.com>
2024-10-22 14:20:18 -0300

    empiezo index, hago primera seccion

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,5 +1,28 @@
+import { Container, Row, Col } from "react-bootstrap";
+import sanJavier from "../../assets/imgInicio/sanJavier.png";
+import "../css/index.css";
+import { useState } from "react";
+import FormularioIndex from "../indexComponents/FormularioIndex";
+
 const Index = () => {
-  return <></>;
+  
+
+  return (
+    <Container>
+      <section className="seccion1">
+        <img src={sanJavier} alt="" className="img-fluid" />
+        <Row className="mt-3">
+          <Col lg="3"className="ms-5">
+            <h3 className="colorVerdeLetra">Buscar Habitaciones</h3>
+            <h6 className="colorVerdeClaro">Tarifas y disponibilidad</h6>
+          </Col>
+          <Col className="">
+            <FormularioIndex></FormularioIndex>
+          </Col>
+        </Row>
+      </section>
+    </Container>
+  );
 };
 
 export default Index;
