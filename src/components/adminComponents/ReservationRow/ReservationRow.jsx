e906c741dd12290759c2f74de72d83998868de04
Leonardo Recalde <leo03recalde@gmail.com>
2024-11-28 17:34:44 -0300

    hago logica para eliminar

--- src/components/adminComponents/ReservationRow/ReservationRow.jsx
+++ src/components/adminComponents/ReservationRow/ReservationRow.jsx
@@ -1,6 +1,8 @@
 import { Button } from "react-bootstrap";
 import style from "../RowUser/RowUser.module.css";
 import { FaRegCalendarCheck, FaPen, FaTrash } from "react-icons/fa";
+import { deleteReservs } from "../../../utils/queris.js";
+import { showCustomAlert } from "../../../utils/customAlert.js";
 
 const ReservationRow = ({
   user_reservation,
@@ -9,6 +11,42 @@ const ReservationRow = ({
   check_out,
   persons,
 }) => {
+  const deleteReservation = async () => {
+    try {
+      showCustomAlert({
+        alertTitle: "¿Seguro de que quieres eliminar la reserva?",
+        alertText: "¡Luego de esta acción no podrás volver atrás!",
+        icon: "warning",
+        showCancel: true,
+        continueConfirm: true,
+        callback: async () => {
+          const respuesta = await deleteReservs(room_reservation.id);
+          if (respuesta.status === 200) {
+            showCustomAlert({
+              alertTitle: "¡La reserva fue eliminada correctamente!",
+              alertText: "",
+              icon: "success",
+            });
+          } else {
+            showCustomAlert({
+              alertTitle: "¡No se pudo eliminar tu reserva!",
+              alertText: "",
+              icon: "danger",
+            });
+          }
+        },
+      });
+    } catch (error) {
+      console.error("Error eliminando la reserva:", error.message);
+      showCustomAlert({
+        alertTitle: "¡Ocurrió un error!",
+        alertText: "No se pudo eliminar la reserva.",
+        icon: "danger",
+      });
+    }
+  };
+  
+  
   return (
     <tr className={` ${style.user_row_container} `}>
       <td className={` ${style.user_icon} `}>
@@ -45,7 +83,7 @@ const ReservationRow = ({
           <Button className={` ${style.action_button} `}>
             <FaPen />
           </Button>
-          <Button className={` ${style.action_button} `}>
+          <Button className={` ${style.action_button} `} onClick={deleteReservation}>
             <FaTrash />
           </Button>
         </div>
--- src/utils/queris.js
+++ src/utils/queris.js
@@ -1,31 +1,43 @@
-export const URL_Usuario = import.meta.env.VITE_API_USUARIO
+export const URL_Usuario = import.meta.env.VITE_API_USUARIO;
+export const URL_Reservation = import.meta.env.VITE_API_RESERVACION;
 
 export const login = async (usuario) => {
   try {
-    const respuesta = await fetch(URL_Usuario+'login', {
+    const respuesta = await fetch(URL_Usuario + "login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(usuario),
     });
-    return respuesta
+    return respuesta;
   } catch (error) {
     console.error(error);
     return { error: "Error en el login" };
   }
 };
 
-export const URL_Reservation = import.meta.env.VITE_API_RESERVACION
-export const crearReserva = async (reserva) =>{
+
+export const crearReserva = async (reserva) => {
   try {
-    const respuesta = await fetch(URL_Reservation,{
+    const respuesta = await fetch(URL_Reservation, {
       method: "POST",
-      headers:{
-        "Content-Type": "application/json"
+      headers: {
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(reserva)
-    })
-  } catch(error){
-    console.error(error)
-    
+      body: JSON.stringify(reserva),
+    });
+  } catch (error) {
+    console.error(error);
   }
-}
+};
+
+export const deleteReservs = async (id) => {
+  try {
+    const respuesta = await fetch(URL_Reservation +'/'+id, {
+      method: "DELETE",
+    });
+    return respuesta;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
